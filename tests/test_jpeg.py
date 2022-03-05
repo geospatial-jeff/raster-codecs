b2c8d3583d348a5929e4fad4452d049a1d6a1960
Jeff Albrecht <geospatialjeff@gmail.com>
2022-03-04 18:03:19 -0700

    make the test better

--- tests/test_jpeg.py
+++ tests/test_jpeg.py
@@ -9,7 +9,7 @@ def test_zarr_compatibility():
     data = np.arange(10000, dtype="uint8").reshape(100, 100)
     compressor = Jpeg(lossless=True)
     z = zarr.array(data, chunks=(1000, 1000), compressor=compressor)
-    numpy.testing.assert_array_almost_equal(z[:256, :256], data[:256, :256])
+    numpy.testing.assert_array_equal(z[:256, :256], data[:256, :256])
 
 
 def test_config_roundtrip():
